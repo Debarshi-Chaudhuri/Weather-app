77d7265259a7a1b81a61ee10e725f7c5b31326e3
Debarshi-Chaudhuri <dchaudhuri17@gmail.com>
2019-08-09 22:03:44 +0530

    first commit

--- src/container/Status.js
+++ src/container/Status.js
@@ -4,14 +4,14 @@ export const Status=(props)=>{
     return(<div>
         <table className="table">
             <tbody>
-            <tr>
-                <td>Geographic coordinates </td>
-                <td>[{props.others.coord.lat}&nbsp;,&nbsp;{props.others.coord.lon}]</td>
-                
-            </tr>
+            
             <tr>
                 <td>Forecast </td>
                 <td>{props.description}</td>
+            </tr>
+            <tr>
+                <td>Temperature </td>
+                <td>{props.others.main.temp}&nbsp;&deg;C</td>
             </tr>
             <tr>
                 <td>Pressure </td>
@@ -20,6 +20,11 @@ export const Status=(props)=>{
             <tr>
                 <td>Humidity </td>
                 <td>{props.others.main.humidity}&nbsp;%</td>
+            </tr>
+            <tr>
+                <td>Geographic coordinates </td>
+                <td>[{props.others.coord.lat}&nbsp;,&nbsp;{props.others.coord.lon}]</td>
+                
             </tr>
             </tbody>
         </table>
